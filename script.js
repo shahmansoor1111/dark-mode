041b7b052703082850204cf3168e614e146c7763
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-08-10 10:09:55 +0500

    ap

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ let btn = document.createElement('button')
 btn.textContent = "Click me"
 document.body.appendChild(btn)
 btn.onclick = function () {
-    if(document.body.style.cssText = "Background-color: yellow") {
-        document.body.style.cssText = "Background-color: black; color:white"
-    }
-    else{
-        document.body.style.backgroundColor = "white"
-    }
+    document.body.style.cssText = " Background-color: black; color:white; text-align: center"
+    // if(document.body.style.cssText = "Background-color: yellow") {
+    //     document.body.style.cssText = "Background-color: black; color:white"
+    // }
+    // else{
+    //     document.body.style.backgroundColor = "white"
+    // }
 }
 for (let i = 1; i <= 50;i++ ) {
     let paras = document.createElement('p')
      paras.textContent = 'i am para' + (i)
      document.body.appendChild(paras)
-      paras.style.cssText = " Background-color: red"
+      paras.style.cssText = " Background-color: red; color:yellow"
     // let para = paras[i]
     paras.addEventListener("click", function () {
-        alert("yo click on " + (i) + "" + "para")
+        alert("yo click on " + (i) + "" + "paragraph")
     })
 
    
