e313ed0a6b6ecbe0f51d7acfbadb73c8f06afeef
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-08-10 10:00:38 +0500

    ap

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let btn = document.createElement('button')
+btn.textContent = "Click me"
+document.body.appendChild(btn)
+btn.onclick = function () {
+    if(document.body.style.cssText = "Background-color: yellow") {
+        document.body.style.cssText = "Background-color: black; color:white"
+    }
+    else{
+        document.body.style.backgroundColor = "white"
+    }
+}
+for (let i = 1; i <= 50;i++ ) {
+    let paras = document.createElement('p')
+     paras.textContent = 'i am para' + (i)
+     document.body.appendChild(paras)
+      paras.style.cssText = " Background-color: red"
+    // let para = paras[i]
+    paras.addEventListener("click", function () {
+        alert("yo click on " + (i) + "" + "para")
+    })
+
+   
+
+
+    
+   
+}
